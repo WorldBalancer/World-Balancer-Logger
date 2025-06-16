b6f9f3d79e9f8b52f997243d113a22c520d63e5e
evl_you6969 <robcraftnet@gmail.com>
2025-06-16 13:46:55 -0400

    Credits to some of the files of preload, main, index.html, webhook/index, TTS of VRCLogger (Project DarkStar) aka NekoSuneVR

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2025 World Balancer
+Copyright (c) 2025 VRCLogger (Project DarkStar)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
